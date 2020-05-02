6918573d2f95b11dbf6b32da0d60de51efb3c69a
Tim Adams <adams.timothy.d@gmail.com>
2020-05-02 14:40:19 -0700

    Add ESlint + airbnb style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
   output: {
     filename: 'myApp.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
         test: /\.jsx?/,
         exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
