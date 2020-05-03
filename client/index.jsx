db9862266289d280fdf4ce3afad96f4f6b7eb3da
Tim Adams <adams.timothy.d@gmail.com>
2020-05-03 10:43:39 -0700

    Add destructuring to state references (airbnb recommendation)

--- client/index.jsx
+++ client/index.jsx
@@ -22,39 +22,58 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // eslint-disable-next-line no-undef
     const params = (new URL(window.location)).searchParams;
+    // eslint-disable-next-line radix
     const id = parseInt(params.get('id'));
-    console.log(id);
 
     $.get(`/${id}`)
       .done((data) => {
         console.log(data);
         this.setState({
-            id: data.info.id,
-            title: data.info.title,
-            publisher: data.info.publisher,
-            contentRating: data.info.content_rating,
-            userRating: data.info.userRating,
-            priceNew: data.info.priceNew,
-            priceUsed: data.info.priceUsed,
-            currentStockNew: data.info.currentStockNew,
-            currentStockUsed: data.info.currentStockUsed,
-            images: data.images,
-            esrbCategory: data.esrb.name,
-            esrbImage: data.esrb.url,
-            });
+          id: data.info.id,
+          title: data.info.title,
+          publisher: data.info.publisher,
+          contentRating: data.info.content_rating,
+          userRating: data.info.userRating,
+          priceNew: data.info.priceNew,
+          priceUsed: data.info.priceUsed,
+          currentStockNew: data.info.currentStockNew,
+          currentStockUsed: data.info.currentStockUsed,
+          images: data.images,
+          esrbCategory: data.esrb.name,
+          esrbImage: data.esrb.url,
+        });
       })
       .fail((err) => console.log(err));
   }
 
   render() {
+    const {
+      id,
+      title,
+      publisher,
+      contentRating,
+      userRating,
+      priceNew,
+      priceUsed,
+      currentStockNew,
+      currentStockUsed,
+      images,
+      esrbCategory,
+      esrbImage,
+    } = this.state;
+
+
     return (
       <div>
-        <h1> You're looking at the info for product { this.state.id } </h1>
-        <img src={this.state.esrbImage} />
-        {this.state.images.map((url) => {
-          return <img src={url} />;
-        })}
+        <h1>
+          You are looking at product
+          {id}
+        </h1>
+        <p />
+        <img src={esrbImage} alt={esrbCategory} />
+        {images.map((url, index) => <img src={url} alt={index + 1} />)}
       </div>
     );
   }
