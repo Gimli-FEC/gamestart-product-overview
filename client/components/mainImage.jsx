3d0cb7d892aac76895829b6df909fdafc5768fa1
Tim Adams <adams.timothy.d@gmail.com>
2020-05-08 10:00:27 -0700

    Add imageSelector, mainImage with zoom, ratings components. Add source-map to webpack config, add skeleton of remaining details component. Add additional endpoint to control images interally to the imageSelector component.

--- client/components/mainImage.jsx
+++ client/components/mainImage.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const MainImg = styled.img`
-  vertical-align: baseline;
+const MainContainer = styled.div`
   width: 600px;
-  heigth: 600px;
-  margin-top: 15px;
+  height: 600px;
+  background-image: url('${(props) => props.src}');
+  background-size: 200% 200%;
+  background-repeat: no-repeat;
+  :hover img {
+    opacity: 0;
+  }
 `;
 
-const MainImage = ({url}) => (
-  <MainImg src={url} alt="I'm the big one!" />
-);
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  pointer-events: none;
+`;
+
+class MainImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundPosition: '0% 0%',
+    };
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  handleMouseMove(e) {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const newX = ((e.pageX - left) / width) * 100;
+    const newY = ((e.pageY - top) / height) * 100;
+    this.setState({ backgroundPosition: `${newX}% ${newY}%` });
+  }
+
+  render() {
+    const { url } = this.props;
+    const { backgroundPosition } = this.state;
+
+    return (
+      <MainContainer onMouseMove={this.handleMouseMove} src={url} style={{ backgroundPosition: `${backgroundPosition}` }}>
+        <Image src={url} />
+      </MainContainer>
+    );
+  }
+}
+
+MainImage.defaultProps = {
+  url: 'https://hrr45-fec.s3.us-east-2.amazonaws.com/assets/white-placeholder.png',
+};
+
+
+MainImage.propTypes = {
+  url: PropTypes.string,
+};
 
 export default MainImage;
---
+++ client/components/ratings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// this is just a stubout of the rating component
+
+const RatingStars = ({ stars }) => (
+  <>
+    {String.fromCharCode(9734) + String.fromCharCode(9734) + String.fromCharCode(9734) + String.fromCharCode(9734) + String.fromCharCode(9734)}
+  </>
+);
+
+export default RatingStars;
