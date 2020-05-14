c207aa86603b0c5e76c3fe89d6109f18ee33f6b8
Tim Adams <adams.timothy.d@gmail.com>
2020-05-13 18:17:54 -0700

    Add origin option to cors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.use(cors());
+app.use(cors({ origin: 'http://127.0.0.1:3000' }));
 
 app.get('/:id', ({ params: { id } }, res) => {
 
